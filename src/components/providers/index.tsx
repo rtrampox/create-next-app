94a9bb8f6666b7671889d3436b12d5c1e11887d9
rtrampox <isaquewrodrigues@icloud.com>
2025-02-24 12:43:13 -0300

    Implement theme provider and utility functions; update Tailwind CSS configuration

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,18 @@
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Toaster } from "sonner";
+
+import { TRPCReactProvider } from "@/trpc/react/client";
+import { ThemeProvider } from "./theme-provider";
+
+export function Providers({ children }: React.PropsWithChildren) {
+	return (
+		<TRPCReactProvider>
+			<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+				<NuqsAdapter>
+					{children}
+					<Toaster richColors theme="system" />
+				</NuqsAdapter>
+			</ThemeProvider>
+		</TRPCReactProvider>
+	);
+}
